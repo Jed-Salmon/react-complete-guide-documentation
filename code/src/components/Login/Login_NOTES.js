6f5f082ff793d0440dd31e71816ec807bdb11942
Jed Salmon <jedsalmon@protonmail.com>
2021-10-28 17:32:08 +0100

    imported useReducer code

---
+++ code/src/components/Login/Login_NOTES.js
@@ -0,0 +1,119 @@
+// useReducer can replace more complex state.
+// A good use case is when you have state that belongs together.
+// AND/OR if you have state that updates depending on other state.
+// Merge your multiple related states into one state with useReducer.
+
+import React, { useState, useEffect, useReducer } from "react";
+
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+// receives two parameters, the state snapshot & the action that was dispatched.
+const emailReducerFn = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  // must return a new state
+  return { value: "", isValid: false };
+};
+// Placed outside of Login function scope because we won't need any data generated inside the component function block.
+
+const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  // initial state and dispatch state
+  const [emailState, dispatchEmail] = useReducer(emailReducerFn, {
+    // useReducers first argument is always a function ^
+    value: "",
+    isValid: null,
+  });
+
+  useEffect(() => {
+    console.log("EFFECT RUNNING");
+
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
+
+  const emailChangeHandler = (event) => {
+    // Takes an action which can be a string, number but are mostly objects.
+    // Our action object takes a type field that describes what happened and an extra payload, which in this case is the value the user has entered.
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+    // dispatchEmail function then triggers the emailReducer function (line 13)
+
+    setFormIsValid(
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
+    );
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+
+    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+  };
+
+  const validateEmailHandler = () => {
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({ type: "INPUT_BLUR" });
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(emailState.value, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
